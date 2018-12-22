aa38ef4ac64c894b93f630ce08442a0e335acc77
DanDPanda <dan.v.dinh@gmail.com>
2018-12-21 22:53:06 -0600

    Added the app.js file to the game.html file and added a function called 'actionBar' to display an action bar in the game.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,6 +33,37 @@ function alertHello(a) {
   console.log(`So yeah, no need for annoying + to concat strings but if you are using variables them use ${a}`);
 }
 
+// Actionbar calculations
+// Uses a recursive timeout to slowly show the time progressing
+// Once one of the characters hits 100, it will break out the loop and allow the player to choose what to do next.
+var playerAB = 0;
+var enemyAB = 0;
+function actionBar() {
+  if (playerAB < 100 && enemyAB < 100) {
+    setTimeout(() => {
+      playerAB += 5;
+      enemyAB += 4;
+      document.getElementById("playerABLog").innerHTML = `Player AB: ${playerAB}/100`;
+      document.getElementById("enemyABLog").innerHTML = `Enemy AB: ${enemyAB}/100`;
+      actionBar();
+    }, 100);
+  } else {
+    if (playerAB >= 100) {
+      playerAB = 0;
+      document.getElementById("attackStatus").innerHTML = "Player attacked the enemy!";
+    } else if (enemyAB >= 100) {
+      enemyAB = 0;
+      document.getElementById("attackStatus").innerHTML = "Enemy attacked the player!";
+    } else {
+      playerAB = 0;
+      enemyAB = 0;
+      document.getElementById("attackStatus").innerHTML = "The player and enemy attacked each other!";
+    }
+    document.getElementById("playerABLog").innerHTML = `Player AB: ${playerAB}/100`;
+    document.getElementById("enemyABLog").innerHTML = `Enemy AB: ${enemyAB}/100`;
+  }
+}
+
 // Console log function
 function consoleLog() {
   console.log('A very ' + 'annoying way ' + 'to concat strings, ' + 'use the first function plox');
